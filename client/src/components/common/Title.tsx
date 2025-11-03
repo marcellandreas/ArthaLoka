b9806c3c74746a094c0d2db1b8a85bfa272b3340
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-11-03 22:43:55 +0700

    feat: implement Title component with customizable titles and paragraph

--- client/src/components/common/Title.tsx
+++ client/src/components/common/Title.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import type { Title } from "../../interface/common";
 
-const Title = () => {
-  return <div>Title</div>;
+const Title = ({
+  title1,
+  title2,
+  titleStyles,
+  title1Styles,
+  paraStyles,
+  para,
+}: Title) => {
+  return (
+    <div className={titleStyles}>
+      <h3 className={`${title1Styles} h-3 capitalize `}>
+        {title1}
+        <span className=" font-medium underline">{title2}</span>
+      </h3>
+      <p className={`${paraStyles} max-w-md mt-2`}>
+        {para
+          ? para
+          : "Discover books that spark curiosity deliver quality and bring inspiration to your everyday reading "}
+      </p>
+    </div>
+  );
 };
 
 export default Title;
