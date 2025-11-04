ceaa66f153d630524e5313f67d9aeaad75e1c28d
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-11-04 22:50:02 +0700

    feat: implement Categories component to display a list of categories with navigation

--- client/src/components/Categories.tsx
+++ client/src/components/Categories.tsx
@@ -1,7 +1,48 @@
-import React from "react";
+import { categories } from "../assets/data";
+import { useShopContext } from "../contexts/ShopContext";
+import Title from "./common/Title";
 
 const Categories = () => {
-  return <div>Categories</div>;
+  const { navigate } = useShopContext();
+  const colors: string[] = [`bg-[#aedae6]`, `bg-[#fff9c9]`, `bg-[#fddbdb]`];
+
+  return (
+    <section className="max-padd-container pt-16">
+      <Title
+        title1="Category"
+        title2="List"
+        titleStyles=""
+        title1Styles="pb-6"
+        paraStyles="hidden"
+        para=""
+      />
+      {/* container */}
+      <div className="flex gap-9 flex-wrap">
+        {categories.map((cat, i) => (
+          <div
+            key={i}
+            onClick={() => navigate(`/shop/${cat.name.toLowerCase()}`)}
+            className="flexCenter flex-col cursor-pointer gruop"
+          >
+            <div
+              className={`flexCenter flex-col h-36 w-36 sm:h-40 sm:w-40 rounded-xl ${
+                colors[i % 3]
+              }`}
+            >
+              <img
+                src={cat.image}
+                alt={cat.name}
+                height={46}
+                width={46}
+                className=" object-cover"
+              />
+              <h5 className="h5 capitalize mt-6">{cat.name}</h5>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Categories;
