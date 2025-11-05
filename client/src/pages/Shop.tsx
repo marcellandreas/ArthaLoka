7e1c5afc01004923d69944e29266a5c4689d5319
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-11-05 23:26:05 +0700

    feat: adding Item Card Book for shop pages

--- client/src/pages/Shop.tsx
+++ client/src/pages/Shop.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Title from "../components/common/Title";
+import { useShopContext } from "../contexts/ShopContext";
+import Item from "../components/Item";
 
 const Shop = () => {
-  return <div>Shop</div>;
+  const { books } = useShopContext();
+  return (
+    <section className=" max-padd-container py-16 pt-28">
+      <Title title1="All" title2="Books" titleStyles="pb-10" />
+      {/* container */}
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 sm:gap-8">
+        {books.length > 0 ? (
+          books.map((book) => <Item book={book} key={book._id} />)
+        ) : (
+          <h4 className="h4">Oops! Nothing matched your search</h4>
+        )}
+      </div>
+    </section>
+  );
 };
 
 export default Shop;
