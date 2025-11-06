55d0fe913a79aad04241a4ce5f598d7ef3ba3e4f
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-11-06 18:27:49 +0700

    feat: implement blog in pages

--- client/src/pages/Blog.tsx
+++ client/src/pages/Blog.tsx
@@ -1,7 +1,28 @@
 import React from "react";
+import { blogs } from "../assets/data";
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="max-padd-container py-16 pt-20">
+      {/* container */}
+      <div className=" grid  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 gap-y-12 pt-6">
+        {blogs.map((blog) => (
+          <div key={blog.title} className=" relative">
+            <img src={blog.image} alt={blog.title} className=" rounded-xl" />
+            <p>{blog.category}</p>
+            <h5 className="h5 mb-1 line-clamp-1">{blog.title}</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Dignissimos nobis vero hic ut quia cumque?
+            </p>
+            <button className=" underline mt-2 bold-14 line-clamp-2 cursor-pointer">
+              Continue Reading
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
