60d5c244d5f2d0df13cfe93e57d76d364e838191
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-11-09 23:33:33 +0700

    feat: CartTotal components

--- client/src/components/cart/CartTotal.tsx
+++ client/src/components/cart/CartTotal.tsx
@@ -1,7 +1,140 @@
-import React from "react";
+import React, { useState } from "react";
+import { useShopContext } from "../../contexts/ShopContext";
+import { dummyAddress } from "../../assets/data";
 
 const CartTotal = () => {
-  return <div>CartTotal</div>;
+  const {
+    navigate,
+    books,
+    cartItems,
+    setCartItems,
+    currency,
+    method,
+    setMethod,
+    getCartAmount,
+    getCartCount,
+    delivery_charges,
+    user,
+  } = useShopContext();
+
+  const [addresses, SetAddresses] = useState(dummyAddress);
+  const [showAddress, SetShowAddress] = useState(false);
+  const [selectedAddress, setSelectedAddress] = useState(dummyAddress[0]);
+  return (
+    <div>
+      <h3 className="bold-22">
+        Order Details
+        <span className="bold-14 text-secondary">({getCartCount()}) Items</span>
+      </h3>
+      <hr className="border-gray-300 my-5" />
+      {/* payment & address */}
+      Payment & Addresses
+      <div className=" mb-5">
+        <div className=" my-5">
+          <h4 className="h4 mb-5">Where to ship your order?</h4>
+          <div className=" relative flex justify-between items-start mt-2">
+            <p>
+              {selectedAddress
+                ? `${selectedAddress.street}, ${selectedAddress.city}, ${selectedAddress.country}`
+                : "No Address found"}
+            </p>
+            <button
+              onClick={() => SetShowAddress(!showAddress)}
+              className=" text-solid text-sm font-medium hover:underline cursor-pointer"
+            >
+              change
+            </button>
+            {showAddress && (
+              <div className=" absolute top-10 py-1 bg-white ring-1 ring-slate-900/10 text-sm w-full">
+                {addresses.map((address, index) => (
+                  <p
+                    key={index}
+                    onClick={() => {
+                      setSelectedAddress(address);
+                      SetShowAddress(false);
+                    }}
+                    className=" p-2 cursor-pointer hover:bg-gray-100 text-sm font-medium"
+                  >
+                    {address.street}, {address.city}, {address.state},{" "}
+                    {address.country}
+                  </p>
+                ))}
+                <p
+                  onClick={() => {
+                    navigate("/address-form");
+                    scrollTo(0, 0);
+                  }}
+                  className=" p-2 text-center cursor-pointer hover:bg-tertiary hover:text-white"
+                >
+                  Add Address
+                </p>
+              </div>
+            )}
+          </div>
+        </div>
+        <hr className="border-gray-300 mt-5" />
+        <div className="my-6">
+          <h4 className="mb-5 h4">Payment Method</h4>
+          <div className=" flex gap-3">
+            <div
+              onClick={() => setMethod("COD")}
+              className={`${
+                method === "COD" ? "btn-secondary" : "btn-light"
+              } py-2 text-xs cursor-pointer`}
+            >
+              Cash on Delivery
+            </div>
+            <div
+              onClick={() => setMethod("stripe")}
+              className={`${
+                method === "stripe" ? "btn-secondary" : "btn-light"
+              } py-2 text-xs cursor-pointer`}
+            >
+              Stripe
+            </div>
+          </div>
+        </div>
+        <hr className="border-gray-300 mt-5" />
+      </div>
+      <div className=" mt-4 space-y-2">
+        <div className="flex justify-between">
+          <h5 className="h5">Price</h5>
+          <p className=" font-bold">
+            {currency}
+            {getCartAmount()}
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <h5 className="h5">Shipping Free</h5>
+          <p className=" font-bold">
+            {currency}
+            {getCartAmount() === 0 ? "$0.00" : `${delivery_charges}.00`}
+          </p>
+        </div>
+        <div className="flex justify-between">
+          <h5 className="h5">Tax (2%)</h5>
+          <p className=" font-bold">
+            {currency}
+            {(getCartAmount() * 2) / 100}
+          </p>
+        </div>
+        <div className="flex justify-between text-lg font-medium mt-3">
+          <h4 className="h4">Total Amount</h4>
+          <p className=" font-bold text-lg">
+            {currency}
+            {getCartAmount() === 0
+              ? "$0.00"
+              : getCartAmount() +
+                delivery_charges +
+                (getCartAmount() * 2) / 100}
+          </p>
+        </div>
+      </div>
+      <button className=" btn-dark w-full mt-8 rounded-md py-2">
+        Proceed to orders
+      </button>
+    </div>
+  );
 };
 
 export default CartTotal;
