1e98ed2f199ac1f27dc49b571438808718cbca44
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-11-10 23:01:55 +0700

    refactor: move isAdminPath logic from App.tsx to MainLayout.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,11 @@
 import { Toaster } from "react-hot-toast";
-import AppRouter from "./routes";
-import MainLayout from "./layouts/MainLayout";
-import { useShopContext } from "./contexts/ShopContext";
-import { useLocation } from "react-router-dom";
 import Login from "./components/auth/Login";
+import { useShopContext } from "./contexts/ShopContext";
+import MainLayout from "./layouts/MainLayout";
+import AppRouter from "./routes";
 
 function App() {
   const { showUserLogin } = useShopContext();
-
-  // const isOwnerPath = useLocation().pathname.includes("owner");
 
   return (
     <main>
