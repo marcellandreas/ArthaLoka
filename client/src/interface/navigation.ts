894ef1694494254a6a7ec306792544e19e63489a
Marcell Andreas Samadhani Duha <marcellandreas.123@gmail.com>
2025-11-12 17:38:04 +0700

    feat: add navigation.ts for interface

---
+++ client/src/interface/navigation.ts
@@ -0,0 +1,10 @@
+export interface NavItemAdmin {
+  path: string;
+  label: string;
+  icon: React.ReactNode;
+}
+export interface NavItemUser {
+  to: string;
+  label: string;
+  icon: React.ReactNode;
+}
